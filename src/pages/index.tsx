f72ca9ed0907e18ced6e6fec6ed85c6119528bfb
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-09-22 17:13:07 -0400

    add random pokemonID fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon"
 import { trpc } from "@/utils/trpc"
 
 export default function Home() {
-  const { data, isLoading } = trpc.hello.useQuery({ text: 'Brian' });
 
-  if (isLoading) return <div>Loading..</div>
-
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon would taste better?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-500" />
+        <div className="w-16 h-16 bg-red-500">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-pink-500" />
+        <div className="w-16 h-16 bg-pink-500">{second}</div>
       </div>
     </div>
   )
