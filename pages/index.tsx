228e8668fbe42ea37c2b9984c8b63db8b93f24d7
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-09-22 16:31:00 -0400

    basic ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <div className="">Hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon would taste better?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-500" />
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-pink-500" />
+      </div>
+    </div>
   )
 }
